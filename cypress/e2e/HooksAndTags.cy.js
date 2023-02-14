63c7c24f3838eabd65ca47c6a2c9a58c3e990453
Ashutoshrx <asutosh007satpathy@gmail.com>
2023-02-14 12:04:48 +0530

    theory on hooks

---
+++ cypress/e2e/HooksAndTags.cy.js
@@ -0,0 +1,8 @@
+/*
+Types of hooks:
+    1.Before-This will be executed only once before all the test cases are executed.
+    2.After-This will be executed only once after all the test cases are executed.
+    3.BeforeEach-This will be executed before each test case.
+    4.AfterEach- This will be executed after each test case.
+*/
+
